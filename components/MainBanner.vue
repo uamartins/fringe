f54d8f39d2282a9662c0d47b1efd198cf9e49cc8
Uelinton Martins <uelintontmartins@gmail.com>
2021-12-21 23:35:47 -0300

    change img

--- components/MainBanner.vue
+++ components/MainBanner.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="bg-[#F8E2C7] h-590px">
         <div class="content flex justify-center mx-auto w-1376px">
-            <img src="/capa_1.png" alt="Capa">
+            <img src="capa_1.png" alt="Capa">
         </div>
         <div class="line"></div>
     </section>
